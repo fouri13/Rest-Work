515d75e38f95755b943a78ae31b4355309f069ee
fouri13 <fabfournier11@gmail.com>
2019-10-23 19:12:42 +0200

    Initial commit

---
+++ store/index.js
@@ -0,0 +1,18 @@
+import cookieparser from 'cookieparser'
+
+export const actions = {  
+  
+  nuxtServerInit({ commit }, { req }) {
+    let user = null
+
+    let cart =[]
+
+    if (req && req.headers && req.headers.cookie) {
+      const parsed = cookieparser.parse(req.headers.cookie)
+      user = (parsed.user && JSON.parse(parsed.user)) || null
+      cart = (parsed.cart && JSON.parse(parsed.cart)) || null
+    }
+
+    commit('auth/setUser', user)
+  }
+}
